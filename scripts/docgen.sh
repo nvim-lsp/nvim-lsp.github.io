e0da3e92a94ad0e1b971391db37baf2a3ef49199
Anders Evenrud <andersevenrud@gmail.com>
2022-01-24 01:44:29 +0100

    chore: update lspconfig submodule path

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
-nvim -u NONE -E -R --headless +'set rtp+=$PWD/src' +'luafile scripts/docgen.lua' +q
+#!/usr/bin/env bash
+set -e
+
+nvim --clean -u NONE -E -R --headless +'set rtp+=$PWD/src/nvim-lspconfig' +'luafile scripts/docgen.lua' +q
+
 python src/generate.py
+
 mkdocs build
